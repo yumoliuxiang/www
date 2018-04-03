1ffe90c39ec9a5254ad50083e5025dbbf33efc6b
cooljser <i@cooljser.com>
2018-04-03 21:00:45 +0800

    使用 raf 减少代码量

--- src/page/home/carousel.js
+++ src/page/home/carousel.js
@@ -1,32 +1,5 @@
 import React from 'react';
-
-(function() {
-	var lastTime = 0;
-    var vendors = ['webkit', 'moz'];
-
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame =
-            window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
-
-    if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function (id) {
-        	clearTimeout(id);
-        };
-    }
-})();
+import raf from 'raf';
 
 export default class Carousel extends React.Component {
 	static defaultProps = {
@@ -44,7 +17,7 @@ export default class Carousel extends React.Component {
 
     componentDidMount = e=>{
         setTimeout(e=>{
-    		requestAnimationFrame(this.run);	
+    		raf(this.run);	
     	}, this.props.interval)
     }
 
@@ -56,10 +29,10 @@ export default class Carousel extends React.Component {
         }
 
         if (this.boxDom.scrollTop % 30) {
-			requestAnimationFrame(this.run);
+			raf(this.run);
         } else {
         	setTimeout(e=>{
-        		requestAnimationFrame(this.run);
+        		raf(this.run);
 	        }, this.props.interval);	
         }
     }
