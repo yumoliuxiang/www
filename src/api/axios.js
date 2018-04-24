37036e1bfcc5071e0e0f186308cec8cf8286718d
zhuweiyou <i@zhuweiyou.com>
2018-04-24 10:19:11 +0800

    增加 User-Token header

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,10 +3,17 @@ import {browserHistory} from 'react-router';
 
 axios.defaults.withCredentials = true;
 
+axios.interceptors.request.use(
+  config => {
+    config.headers.common['User-Token'] = localStorage.getItem('token');
+    return config;
+  },
+  err => Promise.reject(err)
+);
+
 axios.interceptors.response.use(
   res => {
-    let {data} = res;
-
+    const {data} = res;
     //0: 正常
     //1：cookie 不正确或失效
     //4: cookie 不正确，请确保内容包含 ewxshinfo 和 ewxinfo
@@ -14,11 +21,11 @@ axios.interceptors.response.use(
     //10004：邮箱已被注册
     //10005：cookie 已存在
     //10012: 红包链接不正确
-    let codes = [0, 1, 4, 10003, 10004, 10005, 10012];
-
+    const codes = [0, 1, 4, 10003, 10004, 10005, 10012];
     if (codes.includes(data.code)) {
       return data;
-    } else if (data.code === 10000) {
+    }
+    if (data.code === 10000) {
       localStorage.clear();
       browserHistory.push('/login');
     } else {
