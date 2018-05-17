0e23490b44646069bae5c68270712dd53f0f3a97
zhuweiyou <i@zhuweiyou.com>
2018-05-17 13:10:36 +0800

    @cooljser 这块为啥这样写

--- src/api/axios.js
+++ src/api/axios.js
@@ -18,11 +18,12 @@ axios.interceptors.response.use(
     //1：cookie 不正确或失效
     //4: cookie 不正确，请确保内容包含 ewxshinfo 和 ewxinfo
     //10003：验证码错误
+    //10010: 该红包链接已被领取或正在领取中，请换一个红包链接再来！
     //10004：邮箱已被注册
     //10005：cookie 已存在
     //10012: 红包链接不正确
     //10017: 邮箱不存在
-    const codes = [0, 1, 4, 10003, 10004, 10005, 10012, 10017];
+    const codes = [0, 1, 4, 10003, 10004, 10005, 10012, 10017, 10010];
 
     if (codes.includes(data.code)) {
       return data;
