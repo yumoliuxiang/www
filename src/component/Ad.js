041d735374c6c2c249dd469f4ecde0802f196eed
zhuweiyou <i@zhuweiyou.com>
2018-05-17 15:23:14 +0800

    换张图

--- src/component/Ad.js
+++ src/component/Ad.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default () => (
   <a href="https://m.inpeach.com/download?id=dhb_day" target="_blank" rel="noopener noreferrer">
-    <img style={{width: '100%', display: 'block', borderRadius: '4px'}} src={require('../static/ad.jpg')} alt="广告" />
+    <img
+      style={{width: '100%', display: 'block', borderRadius: '4px'}}
+      src={require('../static/ad.jpg')}
+      alt="广告底图"
+    />
   </a>
 );
