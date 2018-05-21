b6a044c5fa0785f79c8e2ea6c5ca402a8763ccb3
zhuweiyou <i@zhuweiyou.com>
2018-05-21 22:35:45 +0800

    下架广告 (#7)

--- src/component/Ad.js
+++ src/component/Ad.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Alert} from 'antd';
 
-export default () => (
-  <a href="https://m.inpeach.com/download?id=dhb_day" target="_blank" rel="noopener noreferrer">
-    <img
-      style={{width: '100%', display: 'block', borderRadius: '4px'}}
-      src={require('../static/ad.jpg')}
-      alt="陪趣广告"
-    />
-  </a>
-);
+export default () => <Alert style={{margin: '15px 0'}} message="广告位招租" type="success" />;
+
+// 有广告时注释上面的 Alert，打开下面的注释
+// export default () => (
+//   <a href="" target="_blank" rel="noopener noreferrer">
+//     <img
+//       style={{width: '100%', display: 'block', borderRadius: '4px'}}
+//       src={require('../static/xxx.jpg')}
+//       alt="某某广告"
+//     />
+//   </a>
+// );
