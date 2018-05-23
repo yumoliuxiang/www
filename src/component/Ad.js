3978370e89512ccc44a238310f460a018cce39be
zhuweiyou <i@zhuweiyou.com>
2018-05-23 21:29:56 +0800

    要倒闭了

--- src/component/Ad.js
+++ src/component/Ad.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import {Alert} from 'antd';
+// import React from 'react';
+// import {Alert} from 'antd';
 
-export default () => <Alert style={{margin: '15px 0'}} message="广告位招租" type="success" />;
+export default () => null;
 
-// 有广告时注释上面的 Alert，打开下面的注释
+// 有广告时打开下面的注释
 // export default () => (
 //   <a href="" target="_blank" rel="noopener noreferrer">
 //     <img
