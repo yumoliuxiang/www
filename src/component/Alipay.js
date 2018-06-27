1ff0824dc413c64b7b14f5a690b971bf4eabd994
zhuweiyou <i@zhuweiyou.com>
2018-06-27 16:18:23 +0800

    更换支付宝红包码

--- src/component/Alipay.js
+++ src/component/Alipay.js
@@ -11,7 +11,6 @@ const Image = styled.div`
   border: 1px solid #ccc;
   padding: 10px;
   border-radius: 4px;
-  width: 262px;
   background: #fff;
   display: none;
 
@@ -33,19 +32,16 @@ const Container = styled.div`
 
 export default class Alipay extends React.Component {
   componentDidMount() {
-    const clipboard = new Clipboard(ReactDOM.findDOMNode(this.refs.button), {
-      text: () => 'c7XYed92oO'
-    });
+    const clipboard = new Clipboard(ReactDOM.findDOMNode(this.refs.button), {text: () => 'aRhixt096d'});
 
     clipboard.on('success', e => {
       e.clearSelection();
-      alert('打开支付宝即可领取红包（每天仅一次）');
+      alert('打开支付宝即可领取红包（每天可以领一次）aRhixt096d');
     });
 
     clipboard.on('error', e => {
       if (window.confirm('您的设备不支持复制红包码，是否跳转到支付宝领取？')) {
-        window.location.href =
-          'https://render.alipay.com/p/f/fd-j6lzqrgm/guiderofmklvtvw.html?shareId=2088312106287923&campStr=p1j%2BdzkZl018zOczaHT4Z5CLdPVCgrEXq89JsWOx1gdt05SIDMPg3PTxZbdPw9dL&sign=CKWXOrsHM0zT9nWHWNo76TOAPo5xqhAzOvXHgBrflIc%3D&scene=offlinePaymentNewSns';
+        window.location.href = 'https://qr.alipay.com/c1x06611gnoczzqiklpka04';
       }
     });
   }
@@ -57,9 +53,9 @@ export default class Alipay extends React.Component {
           支付宝天天领红包
         </Button>
         <Image>
-          <img src={require('../static/alipayhongbao.png')} width="240" alt="支付宝天天领红包" />
+          <img src={require('../static/alipayhongbao.jpg')} width="290" alt="支付宝天天领红包" />
           <div>
-            支付宝天天领红包<br />c7XYed92oO
+            支付宝天天领红包<br />aRhixt096d
           </div>
         </Image>
       </Container>
