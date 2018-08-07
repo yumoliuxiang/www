1a62bcc25493d7aa761c76e15f3374813933a8e8
zhuweiyou <i@zhuweiyou.com>
2018-08-07 09:23:57 +0800

    付费友链到期

--- src/page/Home/FriendLink.js
+++ src/page/Home/FriendLink.js
@@ -3,6 +3,7 @@ import {Icon} from 'antd';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  display: none; /* 需要时删除此行 */
   position: relative;
   border-top: 1px dashed #ccc;
   padding: 15px 0;
@@ -27,8 +28,8 @@ export default class FriendLink extends React.Component {
   state = {
     links: [
       {
-        href: 'https://www.xsery.net/',
-        text: '闲云馆'
+        href: '',
+        text: ''
       }
     ]
   };
@@ -36,9 +37,7 @@ export default class FriendLink extends React.Component {
   render() {
     return (
       <Wrapper>
-        <Icon type="usergroup-add" />&nbsp; {this.state.links.map(link => (
-          <Link href={link.href}>{link.text}</Link>
-        ))}
+        <Icon type="usergroup-add" />&nbsp; {this.state.links.map(link => <Link href={link.href}>{link.text}</Link>)}
       </Wrapper>
     );
   }
