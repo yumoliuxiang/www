3f76a7bcaa94fa55f09e47ef30f0ab19892dc55c
小呆 <2249962820@qq.com>
2018-08-14 14:26:07 +0800

    新增友链申请链接 (#14)

--- src/page/Home/FriendLink.js
+++ src/page/Home/FriendLink.js
@@ -3,7 +3,6 @@ import {Icon} from 'antd';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  display: none; /* 需要时删除此行 */
   position: relative;
   border-top: 1px dashed #ccc;
   padding: 15px 0;
@@ -28,8 +27,8 @@ export default class FriendLink extends React.Component {
   state = {
     links: [
       {
-        href: '',
-        text: ''
+        href: 'http://wpa.qq.com/msgrd?v=3&uin=2249962820&site=qq&menu=yes',
+        text: '申请友链'
       }
     ]
   };
@@ -37,7 +36,11 @@ export default class FriendLink extends React.Component {
   render() {
     return (
       <Wrapper>
-        <Icon type="usergroup-add" />&nbsp; {this.state.links.map(link => <Link href={link.href}>{link.text}</Link>)}
+        <Icon type="usergroup-add" />
+        &nbsp;{' '}
+        {this.state.links.map(link => (
+          <Link href={link.href}>{link.text}</Link>
+        ))}
       </Wrapper>
     );
   }
