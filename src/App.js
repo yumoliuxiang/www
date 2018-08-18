fe0c5f67077cd820302c27b7d5f5e5034751f3a9
zhuweiyou <i@zhuweiyou.com>
2018-08-18 11:19:04 +0800

    移除多余

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {browserHistory, Route, Router} from 'react-router';
-import {message} from 'antd';
 import './globalStyle';
 import './hmt';
 import Mouse from './component/Mouse';
@@ -10,11 +9,6 @@ import Login from './page/Login';
 import Home from './page/Home';
 import ApplyResetPassword from './page/ApplyResetPassword';
 import ResetPassword from './page/ResetPassword';
-import eventbus from './eventbus';
-
-eventbus.on('service-worker:new-version', () => {
-  message.info('新版本已准备好，刷新页面即可更新');
-});
 
 export default () => (
   <div>
