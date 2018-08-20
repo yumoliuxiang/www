fc283b0ac8fbcb7f7a45c46d7a20022170c74659
zhuweiyou <i@zhuweiyou.com>
2018-08-20 09:36:55 +0800

    X-Frame-Options

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-if (window === window.top) {
-  ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-} else {
-  window.top.location.replace('https://www.mtdhb.com');
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
