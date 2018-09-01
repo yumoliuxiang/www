31745bba1c7027b242fff3951b641618660ec109
zhuweiyou <i@zhuweiyou.com>
2018-09-01 22:52:08 +0800

    修复不渲染的问题

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -134,15 +134,19 @@ export default class Home extends React.Component {
     const {carouselRecords = []} = this.state;
     return (
       <div style={{height: '30px', overflow: 'hidden'}}>
-        <Carousel vertical autoplay>
-          {carouselRecords.map((o, i) => (
-            <div key={i} style={{color: '#5bab60', fontSize: '16px', whiteSpace: 'nowrap'}}>
-              {o.mail} 在 {moment(new Date(o.gmtModified)).format('HH:mm:ss')} 领到
-              <span style={{color: '#dd2323'}}>&nbsp;{o.price}&nbsp;</span>
-              元{o.application ? '饿了么' : '美团'}大红包
-            </div>
-          ))}
-        </Carousel>
+        {carouselRecords.length ? (
+          <Carousel vertical autoplay>
+            {carouselRecords.map((o, i) => (
+              <div key={i} style={{color: '#5bab60', fontSize: '16px', whiteSpace: 'nowrap'}}>
+                {o.mail} 在 {moment(new Date(o.gmtModified)).format('HH:mm:ss')} 领到
+                <span style={{color: '#dd2323'}}>&nbsp;{o.price}&nbsp;</span>
+                元{o.application ? '饿了么' : '美团'}大红包
+              </div>
+            ))}
+          </Carousel>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
@@ -319,7 +323,7 @@ export default class Home extends React.Component {
             ) : (
               <div>
                 <span>{`美团 ${meituan.available}/${meituan.total} 次 `}</span>
-                <Icon type="heart" />
+                <Icon type="star" />
                 <span>{` 饿了么 ${ele.available}/${ele.total} 次`}</span>
               </div>
             )
