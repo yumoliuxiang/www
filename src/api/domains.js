44d8b8d2b6cab0611c4861b68487f5340b0de2b5
zhuweiyou <i@zhuweiyou.com>
2018-09-20 10:47:03 +0800

    风云再起

--- src/api/domains.js
+++ src/api/domains.js
@@ -1,10 +1,6 @@
 export default [
   {
     name: '国内线路',
-    value: 'https://mtdhb.z.xxooweb.com/'
-  },
-  {
-    name: '海外线路',
-    value: 'https://api.mtdhb.com/'
+    value: 'https://api.mtdhb.org/'
   }
 ];
