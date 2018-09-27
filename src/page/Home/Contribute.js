282c863bbaa5eca4088e780c86c37de30a30365b
zhuweiyou <i@zhuweiyou.com>
2018-09-27 12:06:24 +0800

    增加提示

--- src/page/Home/Contribute.js
+++ src/page/Home/Contribute.js
@@ -58,7 +58,8 @@ class Contribute extends React.Component {
           1、仅有老用户持有美团绝版 cookie，新号无法贡献美团了<br />
           2、饿了么 cookie 需要验证手机号之后再贡献<br />
           3、失效的 cookie 验证后想要再贡献，需要先删除<br />
-          4、贡献每一个 QQ，需要清除浏览器 cookie 或打开隐身（无痕）模式再登录小号
+          4、贡献每一个 QQ，需要清除浏览器 cookie 或打开隐身（无痕）模式再登录小号<br />
+          5、一个手机号只能为一个 cookie 进行验证，重复验证被我们检测将置为失效小号
         </div>
         <div style={{marginBottom: 5}}>以下两种方式均可贡献饿了么小号：</div>
         <Radio.Group onChange={this.changeContributeMode} value={contributeMode} style={{marginBottom: '12px'}}>
